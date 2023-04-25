e2c110df939429c881ab856b339ef9de67fb8833
Tristan <tristan.couvares@gmail.com>
2023-04-24 17:35:00 -0700

    Update chat_functionality.py

--- chat_functionality.py
+++ chat_functionality.py
@@ -7,21 +7,21 @@ def tokenize(text):
     tokens = tokenizer.encode(text).tokens
     return tokens
 
-def truncate_conversation(conversation, max_tokens=4096):
-    conversation_text = ''.join([msg['content'] for msg in conversation])
-    conversation_tokens = tokenize(conversation_text)
-    if len(conversation_tokens) > max_tokens:
-        conversation_tokens = conversation_tokens[-max_tokens:]
-    truncated_conversation = ''.join(conversation_tokens)
-    return truncated_conversation
-
 def ChatGPT_conversation(conversation):
-    conversation = truncate_conversation(conversation)
-    openai.api_key = "YOURAPIRKEYHERE"
+    # Format the conversation history as a string
+    conversation_history = "\n".join([f"{msg['role']}:{msg['content']}" for msg in conversation])
+
+    # Add a more specific instruction for the model
+    conversation_history += "\n\nassistant: You are a helpful assistant. Please respond to the last message from the user."
+    
+    openai.api_key = "OPENAIKEY"
+
+    # Replace 'your_model_id_here' with your fine-tuned model's ID
+    model_id = "MODELID"
 
     response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt="\n".join([msg["content"] for msg in conversation]),
+        engine=model_id,
+        prompt=conversation_history,
         max_tokens=150,
         n=1,
         stop=None,
@@ -30,7 +30,6 @@ def ChatGPT_conversation(conversation):
 
     assistant_message = response.choices[0].text.strip()
     return assistant_message.strip()
-
 
 def completed_assistant(user_message, conversation):
     conversation.append({"role": "user", "content": user_message})
